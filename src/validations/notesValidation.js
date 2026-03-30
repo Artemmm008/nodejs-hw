f5f0f20c7cc48273e4118d3d31800588fa9847dc
Artemmm008 <artemshkola141@gmail.com>
2026-03-30 21:39:55 +0100

    fixed 2

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -6,7 +6,7 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(TAGS),
+    tag: Joi.string().valid(...Object.values(TAGS)),
     search: Joi.string().trim().allow('')
   }),
 };
@@ -25,7 +25,7 @@ export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(TAGS),
+    tag: Joi.string().valid(...Object.values(TAGS)),
   }),
 };
 
@@ -36,7 +36,7 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(TAGS),
+    tag: Joi.string().valid(...Object.values(TAGS)),
   }).min(1),
 };
 
